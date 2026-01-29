eeb92f72e5a1c911048125665e25c46aa3c05344
dominic-righthere <64965775+dominic-righthere@users.noreply.github.com>
2026-01-30 05:00:45 +0900

    Rename package to reterm-player, publish v0.1.0 to npm

--- packages/player/mdx-test/src/main.tsx
+++ packages/player/mdx-test/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@reterm/player/style.css';
+import 'reterm-player/style.css';
 import Doc from './demo.mdx';
 
 createRoot(document.getElementById('root')!).render(
